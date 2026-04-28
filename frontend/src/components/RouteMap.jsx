b7de37ecea0e946225ac4a99713d0fdc0e6f7c9d
Kordor Pyrbot <opcodegenerator@gmail.com>
2026-04-28 22:55:04 +0530

    Fix Leaflet marker icons on Vercel

--- frontend/src/components/RouteMap.jsx
+++ frontend/src/components/RouteMap.jsx
@@ -1,5 +1,19 @@
 import { GeoJSON, MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { useTheme } from "../theme/useTheme";
+import L from "leaflet";
+
+import "leaflet/dist/leaflet.css";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
 
 /**
  * Renders the planned trip route and its key waypoints.
